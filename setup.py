184d1c910f45d4e5663279e54594d49678809436
JMWhitworth <jack.whitworth@substrakt.co.uk>
2023-03-29 20:30:44 +0100

    Fixed file structure for Pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'zomboid_rcon',
     packages = ['zomboid_rcon'],
-    version = '0.1.2',
+    version = '0.2.0',
     license='GPL-3.0',
     description = 'Python class for interacting with Project Zomboid servers using RCON ',
     author = 'Jack Whitworth',
     author_email = 'jack@jackwhitworth.com',
     url = 'https://jackwhitworth.com',
-    download_url = 'https://github.com/JMWhitworth/zomboid_rcon/archive/refs/tags/0.1.2.tar.gz',
+    download_url = 'https://github.com/JMWhitworth/zomboid_rcon/archive/refs/tags/0.2.0.tar.gz',
     keywords = [
         'project-zomboid',
         'rcon',
