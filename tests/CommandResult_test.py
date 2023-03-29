dc1ec8c46b87c37fad3f1a0e105ebee5c8b66ae0
JMWhitworth <jack.whitworth@substrakt.co.uk>
2023-03-29 19:32:15 +0100

    Tests for CommandResult class

--- tests/CommandResult_test.py
+++ tests/CommandResult_test.py
@@ -1,11 +1,74 @@
 import unittest
-from unittest.mock import MagicMock, Mock
 
 from source.CommandResult import CommandResult
 
 class CommandResult_test(unittest.TestCase):
     def setUp(self):
-        pass
+        self.cr = CommandResult("Default", "Default")
     
     def tearDown(self):
         return super().tearDown()
+    
+    
+    def test_command_returns_expected(self):
+        self.cr.command = "customCommand"
+        self.assertEqual(self.cr.command, "customCommand")
+    
+    def test_command_validates_input(self):
+        values = [0, 0.5, True, None]
+        for value in values:
+            with self.assertRaises(ValueError):
+                self.cr.command = value
+    
+    def test_command_cleans_input(self):
+        self.cr.command = "    customCommand  "
+        self.assertEqual(self.cr.command, "customCommand")
+    
+    
+    def test_failureMessage_returns_expected(self):
+        self.cr.failureMessage = "Custom failure message"
+        self.assertEqual(self.cr.failureMessage, "Custom failure message")
+    
+    def test_failureMessage_validates_input(self):
+        values = [0, 0.5, True, None]
+        for value in values:
+            with self.assertRaises(ValueError):
+                self.cr.failureMessage = value
+    
+    def test_failureMessage_cleans_input(self):
+        self.cr.failureMessage = "    customFailureMessage  "
+        self.assertEqual(self.cr.failureMessage, "customFailureMessage")
+    
+    
+    def test_response_returns_expected(self):
+        self.cr.response = "Response"
+        self.cr.successful = True
+        self.assertEqual(self.cr.response, "Response")
+        
+        self.cr.successful = False
+        self.assertEqual(self.cr.response, self.cr.failureMessage)
+    
+    def test_response_validates_input(self):
+        values = [0, 0.5, True, None]
+        for value in values:
+            with self.assertRaises(ValueError):
+                self.cr.response = value
+    
+    def test_response_cleans_input(self):
+        self.cr.response = "    Response  "
+        self.cr.successful = True
+        self.assertEqual(self.cr.response, "Response")
+    
+    
+    def test_successful_returns_expected(self):
+        self.cr.successful = True
+        self.assertEqual(self.cr.successful, True)
+        
+        self.cr.successful = False
+        self.assertEqual(self.cr.successful, False)
+    
+    def test_successful_validates_input(self):
+        values = [0, 0.5, "Test", None]
+        for value in values:
+            with self.assertRaises(ValueError):
+                self.cr.successful = value
